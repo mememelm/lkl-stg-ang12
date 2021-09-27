bf89b3bb78d6aea8347b4b2fc74264a7f4e4251e
mememelm <carol.arivony@gmail.com>
2021-09-27 18:06:57 +0300

    animation global

--- src/app/pages/agency/agency.component.ts
+++ src/app/pages/agency/agency.component.ts
@@ -1,9 +1,11 @@
+import { ngIfAnimation } from './../../animations/ng-if-animation';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-agency',
   templateUrl: './agency.component.html',
-  styleUrls: ['./agency.component.scss']
+  styleUrls: ['./agency.component.scss'],
+  animations: [ngIfAnimation]
 })
 export class AgencyComponent implements OnInit {
 
