a6593943dd7fda9c1f4bacd31675820054b33d84
mememelm <carol.arivony@gmail.com>
2021-09-27 17:46:25 +0300

    gestion user connected display head bar

--- src/app/components/shared/head-bar/head-bar.component.ts
+++ src/app/components/shared/head-bar/head-bar.component.ts
@@ -1,3 +1,5 @@
+import { Company } from './../../../constants/models/company';
+import { Agency } from './../../../constants/models/agency';
 import { ControllerService } from './../../../services/controller.service';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { User } from 'src/app/constants/models/user';
@@ -12,11 +14,15 @@ export class HeadBarComponent implements OnInit {
   @Output() emitSide = new EventEmitter<any>()
   @Input() toogle = true
   user!: User
+  agency!: Agency
+  company!: Company
 
   constructor(public ctrl: ControllerService) { }
 
   ngOnInit(): void {
     this.user = this.ctrl.storage.user()
+    this.agency = this.ctrl.storage.agency()
+    this.company = this.ctrl.storage.company()
   }
 
   showMenu() {
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -7,11 +7,19 @@ export class StorageService {
 
   constructor() { }
 
+  public token() {
+    return <string>localStorage.getItem('ACCESS_TOKEN')
+  }
+
   public user() {
     return JSON.parse(<string>localStorage.getItem('CURRENT_USER'))
   }
 
-  public token() {
-    return <string>localStorage.getItem('ACCESS_TOKEN')
+  public agency() {
+    return JSON.parse(<string>localStorage.getItem('AGENCY'))
+  }
+
+  public company() {
+    return JSON.parse(<string>localStorage.getItem('COMPANY'))
   }
 }
