5541ef01788b9c515134fe0dd1fcb3a78ac6ac5f
mememelm <carol.arivony@gmail.com>
2021-09-30 14:34:14 +0300

    CSS table et company list

--- src/app/constants/classes/endpoints.ts
+++ src/app/constants/classes/endpoints.ts
@@ -6,4 +6,8 @@ export class EndPoints {
   public static AGENCY_GET = 'agency/get/'
   public static AGENCY_UPDATE = 'agency/update/'
   public static AGENCY_DELETE = 'agency/delete/'
+  public static COMPANY_ADD = 'company/add/'
+  public static COMPANY_GET = 'company/get/'
+  public static COMPANY_UPDATE = 'company/update/'
+  public static COMPANY_DELETE = 'company/delete/'
 }
--- src/app/pages/company/company.component.ts
+++ src/app/pages/company/company.component.ts
@@ -1,16 +1,60 @@
+import { EndPoints } from './../../constants/classes/endpoints';
+import { frenchDataTable } from './../../constants/languages/french-datatable';
+import { Company } from './../../constants/models/company';
+import { Subject } from 'rxjs';
+import { DataTableDirective } from 'angular-datatables';
+import { ngIfAnimation } from './../../animations/ng-if-animation';
 import { ControllerService } from './../../services/controller.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnChanges, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
-  styleUrls: ['./company.component.scss']
+  styleUrls: ['./company.component.scss'],
+  animations: [ngIfAnimation]
 })
-export class CompanyComponent implements OnInit {
+export class CompanyComponent implements OnInit, OnChanges, OnDestroy {
+
+  @ViewChild(DataTableDirective, { static: false }) dtElement!: DataTableDirective
+  dtOptions: DataTables.Settings = {}
+  dtTrigger = new Subject<any>()
+  listCompany: Company[] = []
 
   constructor(public ctrl: ControllerService) { }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      language: frenchDataTable,
+      pagingType: 'full_numbers',
+      lengthMenu: [
+        [5, 10, 50, -1],
+        [5, 10, 50, 'Tous']
+      ],
+      order: [[0, 'ASC']]
+    }
+    this.loadCompany()
+  }
+
+  ngOnChanges(): void {
+    this.loadChange()
+  }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe()
+  }
+
+  loadCompany() {
+    this.ctrl.api.get(EndPoints.COMPANY_GET).subscribe((company: Company[]) => {
+      this.listCompany = company
+      this.dtTrigger.next()
+    })
+  }
+
+  loadChange() {
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.destroy()
+      this.loadCompany()
+    })
   }
 
   openAddCompany(): void {
